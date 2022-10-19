0746ee43637ea8c932fa7bbe8acffda292856b03
SerSo0o <adamserwon@gmail.com>
2022-10-19 22:30:39 +0200

    19.10

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,18 @@
-from flask import Blueprint
+from crypt import methods
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
     
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sing_up():
-    return "<p>sign-up</p>"
+    
+    return render_template("sign_up.html")
 
